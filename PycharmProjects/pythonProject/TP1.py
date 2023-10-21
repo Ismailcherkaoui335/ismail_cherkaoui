8f2fa2fe5cabc6f19972fafeb5a1f2851ad5946c
Ismail Cherkaoui <ismail.cherkaoui335@gmail.com>
2023-10-21 14:42:11 +0100

    update

--- PycharmProjects/pythonProject/TP1.py
+++ PycharmProjects/pythonProject/TP1.py
@@ -1,58 +1,38 @@
 import math
-class Weapon:
-    def __int__(self,ammunitions,range):
-        self.ammunitions = ammunitions
-        self.range = range
 
-    def fire_at(self,x,y,z):
+class Weapon:
+    def __init__(self, ammunitions, weapon_range):
+        self.ammunitions = ammunitions
+        self.weapon_range = weapon_range
+
+    def fire_at(self, x, y, z):
         if z == 0:
-            if math.sqrt(x**2+y**2) > range:
-                ammunitions -=1
+            if math.sqrt(x**2 + y**2) > self.weapon_range:
+                self.ammunitions -= 1
                 raise Exception("OutOfRangeError")
         if z > 0:
-            if math.sqrt(x ** 2 + y ** 2) > range:
-
-                ammunitions -=1
+            if math.sqrt(x**2 + y**2) > self.weapon_range:
+                self.ammunitions -= 1
                 raise Exception("OutOfRangeError")
-
         if z < 0:
-
-
-            if math.sqrt(x ** 2 + y ** 2) > range:
-
-
-
-                ammunitions -= 1
+            if math.sqrt(x**2 + y**2) > self.weapon_range:
+                self.ammunitions -= 1
                 raise Exception("OutOfRangeError")
 
 class Lance_missiles(Weapon):
-    def __init__(self,ammunitions,range,z):
-        super().__init__(ammunitions,range)
-        ammunitions = 50
-        range = 100
+    def __init__(self, ammunitions, weapon_range, z):
+        super().__init__(ammunitions, weapon_range)
         self.z = z
 
 class Lance_missiles_antiair(Weapon):
-    def __init__(self,ammunitions,range,z):
-        super().__init__(ammunitions,range)
-        ammunitions = 40
-        range = 20
+    def __init__(self, ammunitions, weapon_range, z):
+        super().__init__(ammunitions, weapon_range)
         self.z = z
 
 class Lance_torpilles(Weapon):
-    def __init__(self,ammunitions,range,z):
-        super().__init__(ammunitions,range)
+    def __init__(self, ammunitions, weapon_range, z):
+        super().__init__(ammunitions, weapon_range)
         self.z = z
-        ammunitions = 24
-        range = 40
 
-a = Lance_torpilles(50,35,2)
-a.fire_at(3,5,2
-          )
-
-
-
-
-
-
-
+a = Lance_torpilles(50, 35, 2)
+a.fire_at(3, 5, 2)
